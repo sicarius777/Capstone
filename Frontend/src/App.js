c3712785ca17330aaaadfa2fa6c77f816e8b25e6
cameronmcnz <you@example.com>
2024-04-19 15:14:55 -0400

    no _switch

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import LoginPage from './components/LoginPage';
 import MainApp from './components/MainApp';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
